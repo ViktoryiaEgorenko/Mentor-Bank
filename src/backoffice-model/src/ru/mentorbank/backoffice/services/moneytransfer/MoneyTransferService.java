939b3a9bda00b45a11f34c93f88ed9cc49fa0b22
ViktoryiaEgorenko <lavictoria@bk.ru>
2011-12-12 11:39:36 +0400

    Update src/backoffice-model/src/ru/mentorbank/backoffice/services/moneytransfer/MoneyTransferService.java

--- src/backoffice-model/src/ru/mentorbank/backoffice/services/moneytransfer/MoneyTransferService.java
+++ src/backoffice-model/src/ru/mentorbank/backoffice/services/moneytransfer/MoneyTransferService.java
@@ -1,5 +1,6 @@
 package ru.mentorbank.backoffice.services.moneytransfer;
 
+import ru.mentorbank.backoffice.dao.exception.OperationDaoException;
 import ru.mentorbank.backoffice.model.transfer.TransferRequest;
 import ru.mentorbank.backoffice.services.moneytransfer.exceptions.TransferException;
 
